4f2a3d56d22cce5dd7c37034bc9af74bd2cb8847
Gururaj.S.Acharya <sacharyagururaj@gmail.com>
2019-09-17 06:17:10 +0530

    Added build.bat and run.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
 @echo off
 echo Starting build...
 call mvn package
+echo.
+echo Generating coverage report
+call jacoco:report
+echo.
+echo Open 'target/site/jacoco/index.html' to view the coverage report
+echo.
+echo To start the web service run the 'run.bat' batch file 
---
+++ run.bat
@@ -0,0 +1,20 @@
+@echo off
+if "%CATALINA_HOME%" == "" (
+    echo Please setup environment variable CATALINA_HOME pointing to Tomcat's base directory
+) else (
+    if exist target/fences-0.1.war (
+        cd target
+        echo Renaming jar file
+        ren fences-0.1.war fences.war
+        cd ..
+        del "%CATALINA_HOME%/webapps/fences" /s /q /f
+        del "%CATALINA_HOME%/webapps/fences.war" /q /f
+        cd target
+        move fences.war "%CATALINA_HOME%/webapps"
+        cd ..
+        echo Deployed war to Tomcat
+        echo Start Tomcat Server for web service to run
+    ) else (
+        echo Run build.bat first
+    )
+)
